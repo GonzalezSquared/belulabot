48db9e9e2f2230242085c98159ca26942a6ddacc
Miguel González Duque <miguelgondu@gmail.com>
2017-08-26 12:41:43 -0500

    finishes the global round function, corrects card-related functions

--- luloapi.py
+++ luloapi.py
@@ -45,9 +45,9 @@ class Card:
     def __repr__(self):
         return '({}, {})'.format(self.kind, self.number)
 
-class DeckOfCards:
+class Deck:
     '''
-    A DeckOfCards object represents a usual spanish deck (four pints: oro, copa, espada and basto)
+    A Deck object represents a usual spanish deck (four pints: oro, copa, espada and basto)
     and 12 numbers for every card.
     To-Do:
         - What if there's no more cards?
@@ -66,6 +66,148 @@ class DeckOfCards:
         card = random.choice(self.current_deck)
         self.current_deck.remove(card)
         return card
+
+def compare_cards_of_same_kind(card1, card2, showcard):
+    '''
+    This function compares two cards of the same kind. It considers the 
+    whole 7-as-showcard deal. 
+    '''
+
+    if card1.kind != card2.kind:
+        raise ValueError('Cards must be of the same kind')
+
+    if card1 == card2:
+        # print('cards are equal, so return True')
+        return card1
+
+    # The number 7 card of the showcard kind plays as the showcard
+    if card1.kind == showcard.kind:
+        if card1.number == 7 and (card1.number < showcard.number or showcard.number in [1,3]):
+            if compare_cards_of_same_kind(showcard, card2, showcard) == showcard:
+                return card1
+    if card2.kind == showcard.kind:
+        if card2.number == 7 and (card2.number < showcard.number or showcard.number in [1,3]):
+            if compare_cards_of_same_kind(card1, showcard, showcard) == showcard:
+                return card2
+
+    if card1.number == 1:
+        return card1
+    if card2.number == 1:
+        return card2
+
+    if card1.number == 3:
+        return card1
+    if card2.number == 3:
+        return card2
+    # What rests is just making a normal comparison, once 1, 3 and 7 have been
+    # discarded
+
+    if card1.number > card2.number:
+        return card1
+    if card2.number > card1.number:
+        return card2
+
+def compare_cards(list_of_cards, showcard):
+    '''
+    This function compares the list of cards depending on what the showcard and
+    the right-to-dealer card is. It returns the one that should win the round.
+
+    The list of cards is assumed to be in order (i.e. the first element is the rh_card)
+    '''
+    rh_card = list_of_cards[0]
+    current_winning_card = rh_card
+    for card in list_of_cards:
+        # print('card\'s type: ' + str(type(card)))
+        # print('showcard\'s type: ' + str(type(showcard)))
+        # print('rh_card\'s type: ' + str(type(rh_card)))
+        if card.kind == current_winning_card.kind:
+            # print('Comparing {} with {}'.format(card, current_winning_card))
+            current_winning_card = compare_cards_of_same_kind(card, current_winning_card, showcard)
+            # print(current_winning_card)
+            # print('winning card\'s type: ' + str(type(current_winning_card)))
+            # print('Winner is: {}'.format(current_winning_card))
+        if card.kind == showcard.kind and current_winning_card.kind != showcard.kind:
+            # print('Comparing {} with {}'.format(card, current_winning_card))
+            current_winning_card = card
+            # print('Winner is: {}'.format(current_winning_card))
+    return current_winning_card
+
+def is_card_playable(card, hand, showcard, past_cards, round_type=None):
+    '''
+    This function detemines if a player can play a certain card, i.e. he's not
+    "surrendering" by leaving a card of the same kind of the showcard or the
+    rh_card.
+    '''
+    if card not in hand:
+        raise ValueError('Card must be in the player\'s hand')
+
+    list_of_showcard_kind_cards = [c for c in hand if c.kind == showcard.kind]
+    playable_cards = []
+
+    if past_cards == []:
+        '''
+        Then the person is the rh_player (or winner of past rounds). i.e., he
+        plays first:
+        '''
+        if round_type == 'head':
+            if Card(1, showcard.kind) not in hand:
+                return True
+            if Card(1, showcard.kind) in hand and card != Card(1, showcard.kind):
+                return False
+            if Card(1, showcard.kind) in hand and card == Card(1, showcard.kind):
+                return True
+        if round_type == 'body':
+            if list_of_showcard_kind_cards == []:
+                return True
+            if list_of_showcard_kind_cards != []:
+                is_the_best_card = True
+                for _card in list_of_showcard_kind_cards:
+                    is_the_best_card = (is_the_best_card 
+                                        and card == compare_cards_of_same_kind(card, _card))
+        if round_type == 'tail':
+            return True
+        if round_type == None:
+            raise ValueError('List of past cards shouldn\'t be empty.')
+    
+    rh_card = past_cards[0]
+
+    list_of_rh_kind_cards = [c for c in hand if c.kind == rh_card.kind]
+    if list_of_rh_kind_cards != []:
+        for _card in list_of_rh_kind_cards:
+            # Play to win: if the card wins, it is playable.
+            if compare_cards(past_cards + [_card], showcard) != compare_cards(past_cards, showcard):
+                # print('card ' + str(_card) + ' affects!')
+                playable_cards.append(_card)
+        if playable_cards == []:
+            # If no rh-like-card wins, then they all can be played.
+            print('No card affected!')
+            playable_cards += list_of_rh_kind_cards
+            print('Playable cards: ' + str(playable_cards))
+
+        if card in playable_cards:
+            return True
+        else:
+            return False
+
+    if list_of_rh_kind_cards == [] and list_of_showcard_kind_cards != []:
+        for _card in list_of_showcard_kind_cards:
+            if compare_cards(past_cards + [_card], showcard) != compare_cards(past_cards, showcard):
+                playable_cards.append(_card)
+            if playable_cards == []:
+                playable_cards += list_of_showcard_kind_cards
+
+        if card in playable_cards:
+            return True
+        else:
+            return False
+
+    if list_of_rh_kind_cards == [] and list_of_showcard_kind_cards == []:
+        return True
+
+def get_playable_cards(hand, showcard, rh_card, past_cards):
+    playable_cards = [card for card in hand if is_card_playable(card)]
+    return playable_cards
+
 
 class Player:
     '''
@@ -167,14 +309,14 @@ class Player:
             list_of_cards_to_be_changed = bot.select_cards_for_change(gro)
             # Same as above, how do we deal with the info flow?
 
-    def play_card(self, global_round_object, list_of_played_cards):
+    def play_card(self, global_round_object, list_of_played_cards, round_type):
         '''
         This function is for the bot (or human) to decide.
         '''
         gro = global_round_object
         if self.type[0] == 'random' or self.type[0] == None:
             for card in self.hand:
-                if is_card_playable(card):
+                if is_card_playable(card, self.hand, gro.showcard, list_of_played_cards, round_type):
                     self.hand.remove(card)
                     return card
 
@@ -189,7 +331,7 @@ class Player:
             print('Which card do you want to play?, write the index:')
             index = int(input())
             while not is_card_playable(self.hand[index], self.hand, gro.showcard,
-                                    list_of_played_cards[0], list_of_played_cards):
+                                    list_of_played_cards, round_type):
                 print('You can\'t play that, write another index:')
                 index = int(input())
             
@@ -199,9 +341,9 @@ class Player:
             bot = self.type[1]
             # How do we deal with the information flow towards the bot.
             card = bot.play_card(self.hand[index], self.hand, gro.showcard,
-                                list_of_played_cards[0], list_of_played_cards)
+                                list_of_played_cards, round_type)
             if is_card_playable(self.hand[index], self.hand, gro.showcard,
-                                list_of_played_cards[0], list_of_played_cards):
+                                list_of_played_cards, round_type):
                 return bot.play_card(gro)
             else:
                 raise ValueError('Bot is not well programmed, card is not playable.')
@@ -249,10 +391,11 @@ class Round:
     def __init__(self, _list_of_players, _current_lulo_price):
         self.list_of_players = _list_of_players
         self.current_lulo_price = _current_lulo_price
-        self.deck = DeckOfCards()
-        self.bets = [0, 0, 0]
+        self.deck = Deck()
+        self.bets = {'head': 0, 'body': 0, 'tail': 0}
         self.showcard = self.deck.retrieve_card_at_random()
         self.muerto = [self.deck.retrieve_card_at_random() for k in range(4)]
+        self.is_there_muerto = True
 
     def collect_money_from_lulo_players(self):
         '''
@@ -284,15 +427,9 @@ class Round:
         head_amount = random.choice(range(collected_money))
         body_amount = random.choice(range(collected_money - head_amount))
         tail_amount = collected_money - head_amount - body_amount
-        self.bets = [head_amount, body_amount, tail_amount]
-
-    def change_cards(player_object, request):
-        '''
-        This function changes the cards in the player's hand, given his request.
-
-        To-Do: Implement this beauty
-        '''
-        pass
+        self.bets['head'] = head_amount
+        self.bets['body'] = body_amount
+        self.bets['tail'] = tail_amount
 
     def deal_hands(self):
         '''
@@ -301,121 +438,23 @@ class Round:
         for _player in self.list_of_players:
             _player.recieve_hand(self.deck)
 
-def compare_cards_of_same_kind(card1, card2, showcard):
-    '''
-    This function compares two cards of the same kind. It considers the 
-    whole 7-as-showcard deal. 
-    '''
+    def change_cards(global_round_object, player_object, request):
+        '''
+        This function changes the cards in the player's hand, given his request.
+        The request is a list of indexes which are to be changed in player's hand.
+        It does so IN PLACE, in the player_object.
 
-    if card1.kind != card2.kind:
-        raise ValueError('Cards must be of the same kind')
-
-    if card1 == card2:
-        # print('cards are equal, so return True')
-        return card1
-
-    # The number 7 card of the showcard kind plays as the showcard
-    if card1.kind == showcard.kind:
-        if card1.number == 7 and (card1.number < showcard.number or showcard.number in [1,3]):
-            if compare_cards_of_same_kind(showcard, card2, showcard) == showcard:
-                return card1
-    if card2.kind == showcard.kind:
-        if card2.number == 7 and (card2.number < showcard.number or showcard.number in [1,3]):
-            if compare_cards_of_same_kind(card1, showcard, showcard) == showcard:
-                return card2
-
-    if card1.number == 1:
-        return card1
-    if card2.number == 1:
-        return card2
-
-    if card1.number == 3:
-        return card1
-    if card2.number == 3:
-        return card2
-    # What rests is just making a normal comparison, once 1, 3 and 7 have been
-    # discarded
-
-    if card1.number > card2.number:
-        return card1
-    if card2.number > card1.number:
-        return card2
-
-def compare_cards(list_of_cards, showcard, rh_card):
-    '''
-    This function compares the list of cards depending on what the showcard and
-    the right-to-dealer card is. It returns the one that should win the round.
-    '''
-    if rh_card not in list_of_cards:
-        raise ValueError('The asked card must be in the list somewhere')
-    current_winning_card = rh_card
-    for card in list_of_cards:
-        # print('card\'s type: ' + str(type(card)))
-        # print('showcard\'s type: ' + str(type(showcard)))
-        # print('rh_card\'s type: ' + str(type(rh_card)))
-        if card.kind == current_winning_card.kind:
-            # print('Comparing {} with {}'.format(card, current_winning_card))
-            current_winning_card = compare_cards_of_same_kind(card, current_winning_card, showcard)
-            # print(current_winning_card)
-            # print('winning card\'s type: ' + str(type(current_winning_card)))
-            # print('Winner is: {}'.format(current_winning_card))
-        if card.kind == showcard.kind and current_winning_card.kind != showcard.kind:
-            # print('Comparing {} with {}'.format(card, current_winning_card))
-            current_winning_card = card
-            # print('Winner is: {}'.format(current_winning_card))
-    return current_winning_card
-
-def is_card_playable(card, hand, showcard, rh_card, past_cards):
-    '''
-    This function detemines if a player can play a certain card, i.e. he's not
-    "surrendering" by leaving a card of the same kind of the showcard or the
-    rh_card.
-    '''
-    if card not in hand:
-        raise ValueError('Card must be in the player\'s hand')
-
-    list_of_rh_kind_cards = [c for c in hand if c.kind == rh_card.kind]
-    list_of_showcard_kind_cards = [c for c in hand if c.kind == showcard.kind]
-    playable_cards = []
-
-    if list_of_rh_kind_cards != []:
-        for _card in list_of_rh_kind_cards:
-            # Play to win: if the card wins, it is playable.
-            if compare_cards(past_cards + [_card], showcard, rh_card) != compare_cards(past_cards, showcard, rh_card):
-                print('card ' + str(_card) + ' affects!')
-                playable_cards.append(_card)
-        if playable_cards == []:
-            # If no rh-like-card wins, then they all can be played.
-            print('No card affected!')
-            playable_cards += list_of_rh_kind_cards
-            print('Playable cards: ' + str(playable_cards))
-
-        if card in playable_cards:
-            return True
-        else:
-            return False
-
-    if list_of_rh_kind_cards == [] and list_of_showcard_kind_cards != []:
-        for _card in list_of_showcard_kind_cards:
-            if compare_cards(past_cards + [_card], showcard, rh_card) != compare_cards(past_cards, showcard, rh_card):
-                playable_cards.append(_card)
-            if playable_cards == []:
-                playable_cards += list_of_showcard_kind_cards
-
-        if card in playable_cards:
-            return True
-        else:
-            return False
-
-    if list_of_rh_kind_cards == [] and list_of_showcard_kind_cards == []:
-        return True
-
-def get_playable_cards(hand, showcard, rh_card, past_cards):
-    playable_cards = [card for card in hand if is_card_playable(card)]
-    return playable_cards
+        To-Do:
+            -Check if ask_for_change is well implemented, in the sense that requests
+             come here well defined.
+        '''
+        gro = global_round_object
+        for index in request:
+            player_object.hand[index] = gro.deck.retrieve_card_at_random()
 
 def play_global_round(list_of_players, current_lulo_price):
     global_round = Round(list_of_players, current_lulo_price)
+    list_of_winners = []
 
     # The round starts, the money is collected from past lulos and the hands
     # are dealt.
@@ -456,14 +495,56 @@ def play_global_round(list_of_players, current_lulo_price):
             right_to_dealer_player = list_of_players[index]
             index_of_rh_player = index
             break
+
+    # Now we create a new list of players in playing order.
+    list_of_players_in_p_order_head = (list_of_not_folded_players[index_of_rh_player:]
+                                + list_of_not_folded_players[:index_of_rh_player])
     
     # Now we know who's starting the game. Now we start with the head.
-    list_of_played_cards_head = [None for active_player in list_of_not_folded_players]
+    list_of_played_cards_head = []
 
-    for index in range(index_of_rh_player + len(list_of_not_folded_players)):
-        index = index % len(list_of_not_folded_players)
-        player = list_of_not_folded_players[index]
-        list_of_played_cards_head[index] = player.play_card()
+    # Each player plays a card
+    for _player in list_of_players_in_p_order_head:
+        list_of_played_cards_head.append(_player.play_card(global_round, 
+                                list_of_played_cards_head, 'head'))
+
+    # We compare the cards and find out which wins
+    winner_card_head = compare_cards(list_of_played_cards_head, showcard)
+
+    # We award the winner his respective money
+    index_of_winner_head = list_of_played_cards.index(winner_card_head)
+    winner_player_head = list_of_played_cards_head[index_of_winner_head]
+    list_of_winners.append(winner_player_head)
+    winner_player_head.recieve_money(global_round.bets['head'])
+    global_round.bets['head'] = 0
+
+    # Now we go to the body round, with the same procedure:
+    list_of_players_in_p_order_body = (list_of_not_folded_players[index_of_winner_head:]
+                                + list_of_not_folded_players[:index_of_winner_head])
+    list_of_played_cards_body = []
+    for _player in list_of_players_in_p_order_body:
+        list_of_played_cards_body.append(_player.play_card(global_round, 
+                                list_of_played_cards_body, 'body'))
+    winner_card_body = compare_cards(list_of_played_cards_body, showcard)
+    index_of_winner_body = list_of_played_cards.index(winner_card_body)
+    winner_player_body = list_of_played_cards_body[index_of_winner_body]
+    list_of_winners.append(winner_player_body)
+    winner_player_body.recieve_money(global_round.bets['body'])
+    global_round.bets['body'] = 0
+
+    # And, finally, the tail:
+    list_of_players_in_p_order_tail = (list_of_not_folded_players[index_of_winner_body:]
+                                + list_of_not_folded_players[:index_of_winner_body])
+    list_of_played_cards_tail = []
+    for _player in list_of_players_in_p_order_tail:
+        list_of_played_cards_tail.append(_player.play_card(global_round, 
+                                list_of_played_cards_tail, 'tail'))
+    winner_card_tail = compare_cards(list_of_played_cards_tail, showcard)
+    index_of_winner_tail = list_of_played_cards.index(winner_card_tail)
+    winner_player_tail = list_of_played_cards_tail[index_of_winner_tail]
+    list_of_winners.append(winner_player_tail)
+    winner_player_tail.recieve_money(global_round.bets['tail'])
+    global_round.bets['tail'] = 0
 
 def game():
     '''
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import luloapi as lapi
 
 def test_take_card_from_deck():
-    deck = lapi.DeckOfCards()
+    deck = lapi.Deck()
     card = deck.retrieve_card_at_random()
     assert card not in deck.current_deck and card in deck.original_deck
 
@@ -33,7 +33,7 @@ def test_multiple_players_recieving_hands():
     player1 = lapi.Player()
     player2 = lapi.Player()
     player3 = lapi.Player()
-    deck = lapi.DeckOfCards()
+    deck = lapi.Deck()
     player1.recieve_hand(deck)
     player2.recieve_hand(deck)
     player3.recieve_hand(deck)
@@ -55,120 +55,112 @@ def test_compare_cards_for_equality():
 def test_compare_cards_for_equality2():
     card1 = lapi.Card('e', 3)
     card2 = lapi.Card('e', 3)
-
     assert card1 == card2
 
 def test_comparison_of_cards1():
     card1 = lapi.Card('o', 3)
     card2 = lapi.Card('o', 6)
-    rh_card = card1
     showcard = lapi.Card('o', 1)
-    assert lapi.compare_cards([card1, card2], showcard, rh_card) == card1
+    assert lapi.compare_cards([card1, card2], showcard) == card1
 
 def test_comparison_of_cards2():
     card1 = lapi.Card('o', 3)
     card2 = lapi.Card('o', 6)
     card3 = lapi.Card('b', 10)
     card4 = lapi.Card('e', 12)
-    rh_card = card1
     showcard = lapi.Card('o', 1)
     assert lapi.compare_cards([card1, card2, card3, card4],
-                              showcard, rh_card) == card1
+                              showcard) == card1
 
 def test_comparison_of_cards3():
     card1 = lapi.Card('b', 10)
     card2 = lapi.Card('o', 6)
     card3 = lapi.Card('b', 1)
     card4 = lapi.Card('e', 12)
-    rh_card = card1
     showcard = lapi.Card('b', 2)
     assert lapi.compare_cards([card1, card2, card3, card4],
-                              showcard, rh_card) == card3
+                              showcard) == card3
 
-def test_comparison_of_cards3():
+def test_comparison_of_cards4():
     showcard = lapi.Card('b', 2)
 
     card1 = lapi.Card('e', 3)
     card2 = lapi.Card('e', 1)
     card3 = lapi.Card('b', 4)
     card4 = lapi.Card('o', 12)
-    rh_card = card1
     
     assert lapi.compare_cards([card1, card2, card3, card4],
-                              showcard, rh_card) == card3
+                              showcard) == card3
 
-def test_comparison_of_cards4():
+def test_comparison_of_cards5():
     showcard = lapi.Card('o', 1)
 
     card1 = lapi.Card('e', 3)
     card2 = lapi.Card('e', 1)
     card3 = lapi.Card('o', 3)
     card4 = lapi.Card('o', 7)
-    rh_card = card1
 
     # Here, card4 should win, because it is playing as the ('o', 1).
 
     assert lapi.compare_cards([card1, card2, card3, card4],
-                              showcard, rh_card) == card4
+                              showcard) == card4
 
-def test_comparison_of_cards5():
+def test_comparison_of_cards6():
     showcard = lapi.Card('c', 3)
 
     card1 = lapi.Card('c', 2)
     card2 = lapi.Card('c', 12)
     card3 = lapi.Card('c', 7)
     card4 = lapi.Card('c', 6)
-    rh_card = card1
 
     assert lapi.compare_cards([card1, card2, card3, card4],
-                              showcard, rh_card) == card3
+                              showcard) == card3
 
-def test_comparison_of_cards6():
+def test_comparison_of_cards7():
     showcard = lapi.Card('c', 3)
 
     card1 = lapi.Card('c', 4)
     card2 = lapi.Card('c', 5)
     card3 = lapi.Card('c', 6)
     card4 = lapi.Card('c', 9)
-    rh_card = card1
 
     assert lapi.compare_cards([card1, card2, card3, card4],
-                              showcard, rh_card) == card4
+                              showcard) == card4
 
 def test_is_card_playable1():
     showcard = lapi.Card('c', 3)
     rh_card = lapi.Card('o', 5)
     hand = [lapi.Card('o', 1), lapi.Card('b', 7), lapi.Card('c', 1)]
     assert lapi.is_card_playable(lapi.Card('o', 1),
-                                 hand, showcard, rh_card, [rh_card])
+                                 hand, showcard, [rh_card])
 
 def test_is_card_playable2():
     showcard = lapi.Card('c', 3)
     rh_card = lapi.Card('o', 5)
     hand = [lapi.Card('b', 1), lapi.Card('b', 7), lapi.Card('c', 1)]
     assert not lapi.is_card_playable(lapi.Card('b', 7),
-                                 hand, showcard, rh_card, [rh_card])
+                                 hand, showcard, [rh_card])
 
 def test_is_card_playable3():
     showcard = lapi.Card('c', 3)
     rh_card = lapi.Card('o', 5)
     hand = [lapi.Card('o', 4), lapi.Card('b', 7), lapi.Card('o', 1)]
     assert not lapi.is_card_playable(lapi.Card('o', 4),
-                                 hand, showcard, rh_card, [rh_card])
+                                 hand, showcard, [rh_card])
 
 def test_is_card_playable4():
     showcard = lapi.Card('c', 3)
     rh_card = lapi.Card('o', 5)
     hand = [lapi.Card('o', 4), lapi.Card('b', 7), lapi.Card('o', 1)]
     assert lapi.is_card_playable(lapi.Card('o', 1),
-                                 hand, showcard, rh_card, [rh_card])
+                                 hand, showcard, [rh_card])
 
 def test_is_card_playable5():
     showcard = lapi.Card('e', 5)
     rh_card = lapi.Card('o', 10)
     hand = [lapi.Card('o', 4), lapi.Card('e', 8), lapi.Card('o', 5)]
     assert lapi.is_card_playable(lapi.Card('o', 4),
-                                 hand, showcard, rh_card, [rh_card])
+                                 hand, showcard, [rh_card])
 
 def test_is_card_playable6():
     showcard = lapi.Card('e', 5)
@@ -176,7 +168,7 @@ def test_is_card_playable6():
     past_cards = [rh_card, lapi.Card('e', 1)]
     hand = [lapi.Card('o', 4), lapi.Card('e', 8), lapi.Card('o', 3)]
     assert lapi.is_card_playable(lapi.Card('o', 4),
-                                 hand, showcard, rh_card, past_cards)
+                                 hand, showcard, past_cards)
 
 def test_is_card_playable7():
     showcard = lapi.Card('e', 5)
@@ -184,4 +176,4 @@ def test_is_card_playable7():
     past_cards = [rh_card, lapi.Card('e', 1)]
     hand = [lapi.Card('c', 4), lapi.Card('e', 8), lapi.Card('c', 3)]
     assert lapi.is_card_playable(lapi.Card('e', 8),
-                                 hand, showcard, rh_card, past_cards)
+                                 hand, showcard, past_cards)
